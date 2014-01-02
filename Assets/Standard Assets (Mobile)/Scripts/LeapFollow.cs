d2dd7870c766802eb9d54defec0e0ac2148d608f
Sang-Jung Lee <sj.lee36bc@gmail.com>
2014-01-01 17:25:38 -0800

    Leap Motion controller added Now just have to get it to move something...

---
+++ Assets/Standard Assets (Mobile)/Scripts/LeapFollow.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using Leap;
+
+public class LeapFollow : MonoBehaviour {
+    Controller m_leapController;
+
+    void Start ()
+    {
+        m_leapController = new Controller();
+    }
+
+    // gets the hand furthest away from the user (closest to the screen).
+	Hand GetForeMostHand() {
+		Frame f = m_leapController.Frame();
+		Hand foremostHand = null;
+		float zMax = -float.MaxValue;
+		for(int i = 0; i < f.Hands.Count; ++i) {
+			float palmZ = f.Hands[i].PalmPosition.ToUnityScaled().z;
+			if (palmZ > zMax) {
+				zMax = palmZ;
+				foremostHand = f.Hands[i];
+			}
+		}
+
+		return foremostHand;
+	}
+
+	void MovePaddle (Hand hand) {
+		if (hand.PalmPosition.ToUnityScaled().z > 0) {
+			transform.position += transform.forward * 0.1f;
+		}
+		
+		if (hand.PalmPosition.ToUnityScaled().z < -1.0f) {
+			transform.position -= transform.forward * 0.04f;
+		}
+	}
+
+    void FixedUpdate () {
+		Hand foremostHand = GetForeMostHand();
+		if (foremostHand != null) {
+    	print("hi");
+			MovePaddle(foremostHand);
+		}
+	}
+}
---
+++ Assets/Standard Assets (Mobile)/Scripts/Reset.js
@@ -0,0 +1,17 @@
+﻿#pragma strict
+
+var startPos : Vector3;
+var startRot : Quaternion;
+
+function Start () {
+    startPos = transform.position;
+    startRot = transform.rotation;
+}
+
+function Update () {
+	if (transform.position.y < -10) {
+		Debug.Log("Reset ball position");
+		transform.position = startPos;
+		transform.rotation = startRot;
+	}
+}
